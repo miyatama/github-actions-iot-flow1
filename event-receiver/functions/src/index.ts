8f8aa3ced7dff54b445861f9eb0fee8cf365a265
naoyuki_miyata <n.miyata080825@gmail.com>
2022-05-07 15:04:52 +0900

    add event receiver(Firebase functions)

---
+++ event-receiver/functions/src/index.ts
@@ -0,0 +1,29 @@
+import * as functions from "firebase-functions";
+import * as Amqp from "amqp-ts";
+
+export const githubActionsEvent =
+  functions.https.onRequest((request, response) => {
+    functions.logger.info("receive data: ", request.body);
+    const connection = new Amqp.Connection("amqp://rabbitmq:rabbitmq@34.84.72.244:5672");
+    const msg = new Amqp.Message(JSON.stringify(request.body));
+    const exchange = connection.declareExchange(
+        "amq.topic",
+        "topic",
+        {durable: true});
+    connection.completeConfiguration().then(() => {
+      exchange.send(msg, "github-actions-event");
+      functions.logger.info("AMQP publish: ", msg);
+
+      setTimeout(function() {
+        connection.close();
+      }, 500);
+      response
+          .status(200)
+          .send("AMQP publish succeed");
+    }).catch((err) => {
+      functions.logger.error("AMQP connection error: ", msg);
+      response
+          .status(500)
+          .send("AMQP connection error");
+    });
+  });
